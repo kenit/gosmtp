4ba0100812f1c22ba57c2c58fc941eeb5df6dc1c
Kenit <kenit@surehigh.com.tw>
2016-11-01 14:12:45 +0800

    add support to TLS

--- gosmtp.go
+++ gosmtp.go
@@ -1,62 +1,83 @@
 package gosmtp
 
-import(
-	"net/smtp"
-	"fmt"
-	"encoding/base64"
-	"time"
+import (
 	"crypto/md5"
+	"crypto/tls"
+	"encoding/base64"
+	"fmt"
+	"net/smtp"
+	"time"
+	"github.com/pborman/uuid"
 )
 
 const QueueSize = 100
 
-type EmailSender struct{
-	ServerAddr string
-	ServerPort int	
+type EmailSender struct {
+	ServerAddr  string
+	ServerPort  int
 	SenderEmail string
-	Username string
-	Password string
-	conn *smtp.Client
-	queue chan *Task
+	Username    string
+	Password    string
+	UseTLS      bool
+	conn        *smtp.Client
+	queue       chan *Task
 }
 
 func (e *EmailSender) AddQueue(t *Task) <-chan error {
 	if e.queue == nil {
-		e.queue = make(chan *Task,QueueSize)
+		e.queue = make(chan *Task, QueueSize)
 	}
-	t.err = make(chan error,1)
+	t.err = make(chan error, 1)
 	e.queue <- t
 	return t.err
 }
 
-func (e *EmailSender) run(done chan <- interface{}) error{
+func (e *EmailSender) run(done chan<- interface{}) error {
 	timer := time.NewTimer(10 * time.Second)
-	if e.conn == nil || e.conn.Hello("localhost")!=nil{
-		var err error
-		if e.conn, err = smtp.Dial(fmt.Sprintf("%s:%d",e.ServerAddr, e.ServerPort));err!=nil{
-			return fmt.Errorf("[EMAIL] SERVER Connect Failed，%s",err)
+	if e.conn == nil || e.conn.Hello("localhost") != nil {
+
+		if e.UseTLS {
+			config := &tls.Config{
+				ServerName: e.ServerAddr,
+			}
+
+			if conn, err := tls.Dial("tcp", fmt.Sprintf("%s:%d", e.ServerAddr, 465), config); err != nil {
+				return fmt.Errorf("[EMAIL] SERVER Connect Failed，%s", err)
+			} else {
+				var err error
+				if e.conn, err = smtp.NewClient(conn, e.ServerAddr); err != nil {
+					return fmt.Errorf("[EMAIL] SERVER Auth failed，%s", err)
+				}
+			}
+
+		} else {
+
+			var err error
+			if e.conn, err = smtp.Dial(fmt.Sprintf("%s:%d", e.ServerAddr, e.ServerPort)); err != nil {
+				return fmt.Errorf("[EMAIL] SERVER Connect Failed，%s", err)
+			}
 		}
-		if e.Username != ""{
+		if e.Username != "" {
 			auth := smtp.PlainAuth("", e.Username, e.Password, e.ServerAddr)
-			if err := e.conn.Auth(auth);err !=nil {
-				return fmt.Errorf("[EMAIL] SERVER Auth failed，%s",err)
+			if err := e.conn.Auth(auth); err != nil {
+				return fmt.Errorf("[EMAIL] SERVER Auth failed，%s", err)
 			}
 		}
 	}
-	go func(){
-		loop:
-		for{
-			select{
-				case t := <-e.queue:				
-					if err := e.send(t);err != nil{
-						select{
-							case t.err <- err:
-							case <- timer.C:
-						}
+	go func() {
+	loop:
+		for {
+			select {
+			case t := <-e.queue:
+				if err := e.send(t); err != nil {
+					select {
+					case t.err <- err:
+					case <-timer.C:
 					}
-					close(t.err)
-				case <- timer.C:
-					break loop
+				}
+				close(t.err)
+			case <-timer.C:
+				break loop
 			}
 			timer.Reset(10 * time.Second)
 		}
@@ -67,50 +88,53 @@ func (e *EmailSender) run(done chan <- interface{}) error{
 	return nil
 }
 
-func (e *EmailSender) send(t *Task) error{
+func (e *EmailSender) send(t *Task) error {
 	if err := e.conn.Mail(e.SenderEmail); err != nil {
-		return fmt.Errorf("[EMAIL] Server can't accept sender，%s，%s",e.SenderEmail,err)
+		return fmt.Errorf("[EMAIL] Server can't accept sender，%s，%s", e.SenderEmail, err)
 	}
-	for _, r := range(t.To){
-		if err := e.conn.Rcpt(r);err != nil{
-			return fmt.Errorf("[EMAIL] Server can't accept recipient，%s，%s",r,err)
+	for _, r := range t.To {
+		if err := e.conn.Rcpt(r); err != nil {
+			return fmt.Errorf("[EMAIL] Server can't accept recipient，%s，%s", r, err)
 		}
 	}
-	if wc,err:=e.conn.Data();err == nil{
-		boundary := fmt.Sprintf("%x",md5.Sum(t.Content))
-		wc.Write([]byte("Subject: " + "=?UTF-8?B?" + base64.StdEncoding.EncodeToString([]byte(t.Subject)) +"?=\n"))
+	if wc, err := e.conn.Data(); err == nil {
+		boundary := fmt.Sprintf("%x", md5.Sum(t.Content))
+		wc.Write([]byte("Message-ID:<" + uuid.New() + "@hotelnabe.com.tw>\n"))
+		wc.Write([]byte("Subject: " + "=?UTF-8?B?" + base64.StdEncoding.EncodeToString([]byte(t.Subject)) + "?=\n"))
 		wc.Write([]byte("MIME-Version: 1.0" + "\n"))
 		wc.Write([]byte("Date: " + time.Now().Format(time.RFC1123Z) + "\n"))
 		wc.Write([]byte("From: " + e.SenderEmail + "\n"))
 		wc.Write([]byte("To: undisclosed-recipients:;\n"))
-		wc.Write([]byte("Content-Type: multipart/mixed; boundary = b" + boundary + "\n"))
+		wc.Write([]byte("Content-Type: multipart/mixed; boundary=b" + boundary + "\n\n"))
 		wc.Write([]byte("This is a multi-part message in MIME format.\n\n--b" + boundary + "\n"))
-		wc.Write([]byte("Content-Type: text/html;charset=uft-8\n"))
+		wc.Write([]byte("Content-Type: text/html;charset=UTF-8\n"))
 		wc.Write([]byte("Content-Transfer-Encoding: base64\n\n"))
 		body := base64.StdEncoding.EncodeToString(t.Content)
-		for len(body)>76{
+		for len(body) > 76 {
 			wc.Write([]byte(body[0:76]))
 			wc.Write([]byte("\n"))
-			body = body[76:]		
+			body = body[76:]
 		}
-		wc.Write([]byte(body[0:]))		
-		wc.Write([]byte("\n\n--b"+boundary))
+		wc.Write([]byte(body[0:]))
+		wc.Write([]byte("\n\n--b"+boundary+"--"))
 		wc.Close()
+	}else{
+		return err
 	}
 	return nil
 }
 
-func (e *EmailSender) Init() <-chan error{
-	er := make(chan error,1)
-	go func(){
-		done := make(chan interface{},1)
-		for{
-			if len(e.queue)>0{
-				if err := e.run(done);err!=nil{
+func (e *EmailSender) Init() <-chan error {
+	er := make(chan error, 1)
+	go func() {
+		done := make(chan interface{}, 1)
+		for {
+			if len(e.queue) > 0 {
+				if err := e.run(done); err != nil {
 					er <- err
 					continue
-				}				
-				<- done
+				}
+				<-done
 			}
 			time.Sleep(10 * time.Second)
 		}
@@ -118,9 +142,9 @@ func (e *EmailSender) Init() <-chan error{
 	return er
 }
 
-type Task struct{
+type Task struct {
 	Subject string
-	To []string
+	To      []string
 	Content []byte
-	err chan error
+	err     chan error
 }
